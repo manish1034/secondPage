62258ee64691fb83e6235c371de0f0387528ecb4
manish1034 <76244041+manish1034@users.noreply.github.com>
2024-05-07 17:35:52 +0530

    commit for responsiveness

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelector('.menu-btn').onclick = () => {
-    const header = document.querySelector('.topbar')
+    const header = document.querySelector('.topbar');
     console.log(header.classList.remove)
     if (header.classList.contains('hide')) {
         header.classList.remove('hide')
